1b81f3318702db1cf9f2f33c7adda191cc3d056e
IvanGolovin <ivan.golovin@jetruby.com>
2019-01-12 18:53:49 +0300

    - Added icons.

--- app/src/main/java/com/gis/giscurrenciesapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/gis/giscurrenciesapp/presentation/ui/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.gis.giscurrenciesapp.presentation.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
 import com.gis.giscurrenciesapp.R
+import com.gis.giscurrenciesapp.databinding.ActivityMainBinding
+import com.gis.navigation.AppNavigator
+import org.koin.android.ext.android.get
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initBinding()
+        initNavController()
+    }
+
+    private fun initBinding() {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
+    private fun initNavController() {
+        val mainNavController =
+            (supportFragmentManager.findFragmentById(R.id.main_nav_host) as NavHostFragment).navController
+        val navigator: AppNavigator = get()
+        navigator.setNavController(mainNavController)
     }
 }
